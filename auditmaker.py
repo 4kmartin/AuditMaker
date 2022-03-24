f993cfc78f4aba103903b8067c8410ad5800cfd7
Anthany Martin <4kmartin@gmail.com>
2022-03-24 15:50:03 -0500

    added License Statement

--- auditmaker.py
+++ auditmaker.py
@@ -1,3 +1,21 @@
+"""
+auditmaker.py automate the construction of tenable audit files
+    Copyright (C) 2022  Anthany Martin
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2 as published by
+    the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
 
 import CSV_to_audit_file
 from sys import argv
@@ -10,6 +28,12 @@ def main (args):
             -h or --help            Display this message
 
             -csv [path to file]     generate a  audit file from the contents of a csv
+
+        License Statement:
+                auditmaker.py, Copyright (C) 2022 Anthany Martin
+            auditmaker.py comes with ABSOLUTELY NO WARRANTY.
+            This is free software, and you are welcome to redistribute it
+            under certain conditions; see the included license for details.
     """
 
     if  args [1] in ("-h", "--help"):
